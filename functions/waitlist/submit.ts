68c89af919208462e3c0c7b7c88dd94f30e8c508
Spencer Scorcelletti <contact@spenc.es>
2023-05-12 01:44:55 +0200

    Add a cloudflare d1 database binding to store each waitlist form submission

--- functions/waitlist/submit.ts
+++ functions/waitlist/submit.ts
@@ -1,9 +1,16 @@
+interface Env {
+  COOP_DB: D1Database;
+}
+
 export function onRequest(context) {
   if (context.request.method === "POST") {
     const fd = context.request.formData()
     return fd.then(fd => {
       const entries = Object.fromEntries(fd)
-      return new Response(`${entries.name}, ${entries.email}, ${entries.message}`)
+      const ps = context.env.COOP_DB
+        .prepare('INSERT INTO waitlist (name, email, message) VALUES (?1, ?2, ?3)')
+      const result = ps.bind(entries.name, entries.email, entries.message).run()
+      return result.then(db_rep => new Response(`${db_rep}`))
     })
   } else {
     return new Error("Method not allowed")
