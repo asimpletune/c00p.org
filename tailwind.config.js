086ebd61a1c656a49477418b95795362295a220b
Spencer Scorcelletti <contact@spenc.es>
2023-05-11 15:37:49 +0200

    Remove template repo images and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
   content: ['./templates/**/*.html', './static/**/*.{html,js}'],
   theme: {
     extend: {},
-    backgroundImage: {
-      'beams-dark': 'url("/beams.webp"), linear-gradient(#0D172B, #12213F)',
-      'beams-light': 'url("/beams.webp"), linear-gradient(#CDFFF9, #FFDCDF)',
-    }
+    backgroundImage: {}
   },
   plugins: [
     require('@tailwindcss/typography'),
