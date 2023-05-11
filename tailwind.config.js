fb2f668208262200918947f04b013d65ed42825b
Spencer Scorcelletti <contact@spenc.es>
2023-05-11 18:34:39 +0200

    Add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./templates/**/*.html', './static/**/*.{html,js}'],
   theme: {
     extend: {},
-    backgroundImage: {}
+    backgroundImage: {},
+    fontFamily: {
+      'playfair-display': ['Playfair Display', ...defaultTheme.fontFamily.serif],
+      'fira-sans': ['Fira Sans', ...defaultTheme.fontFamily.sans],
+    }
   },
   plugins: [
     require('@tailwindcss/typography'),
